f44d7ba9530090578068370f3cf57f4f1c068229
sofia-cabrera <sofia.cabrera1996@gmail.com>
2022-08-25 09:46:07 -0300

    Últimos avances
    Esto contiene la últimas semanas de avances, necesito mejorar mi forma de subir regularmente mis avances por aquí

--- js/login.js
+++ js/login.js
@@ -2,11 +2,13 @@
 //para que el botón Ingresar redireccione a la portada
 
 const email= document.getElementById ('email_addr');
-const contrasenia= document.getElementById ('password');
+const password= document.getElementById ('password');
 
 function redireccionar(){
-    if ((email.value != "") && (contrasenia.value != "")) {
+    if ((email.value != "") && (password.value != "")) {
         window.location.href = "portada.html";
     }
-    
 }
+
+//necesito guardar en el localStorage el objeto/valor de email
+//para recuperarlo en la barra de navegación, aparezca a la derecha siempre 
--- js/products.js
+++ js/products.js
@@ -1,26 +1,36 @@
-/*petición web a una URL donde se encuentra la colección
-de productos en formato json(categoria 101 Autos) con la info 
+/*petición web a una URL donde se encuentran las colecciónes
+de productos en formato json(de cada categoria) con la info 
 (precio, nombre, descripción, cantidad vendidos e imagen)
 de cada producto y mostrar el listado products.html*/
 
 /*Acá lo hice para que mostrara la categoria Autos, pero la mostraba en cualquier categoria
 a la que le hicieras click
 const AUTOS_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json"; */
-const listaPRODUCTO_URL = "https://japceibal.github.io/emercado-api/cats_products/" + localStorage.getItem("catID") +".json";
+const listaPRODUCTO_URL = "https://japceibal.github.io/emercado-api/cats_products/" + localStorage.getItem("catID") + EXT_TYPE;
 /* con esta const logro que me dirija al json correspondiente de la categoria a la que se le hace click*/
-let listado = [];/*declaro una variable vacia para poner dentro los datos que obtengo*/
+let listado = [];/*declaro uan array vacia para poner dentro los datos que obtengo*/
 
+//reutilicé el código de categorias, y no quise desaprovechaar cosas que ya me quedaban
+//eso incluye la función setProductID, que me permite redirigir a la info del producto cuando haga click
+function setProductID(id) {
+    localStorage.setItem("ProductID", id);
+    window.location = "product-info.html"//funcionalidad en desarrollo
+}
+
+/*AGREGAR FILTROS
+Aplicar filtros a partir de rango de precio definido.
+Agregar las funcionalidades de orden ascendente y descendente en función del precio
+y descendente en función de la relevancia (tomaremos para ello la cantidad de artículos vendidos)
+
+
+*/
 
 function mostrarProductos() {
 
     let htmlContentToAppend = "";
     for (let i = 0; i < listado.length; i++) {
         let producto = listado[i];
-/*dejé el onclick="setProductID(${product.id})" porque se que me puede servir cuando tenga que trabajar con cada producto
-function setProductID(id) {
-    localStorage.setItem("ProductID", id);
-    window.location = "product-info.html"
-}
+/*dejé el onclick="setProductID(${product.id})" porque se que me va a servir cuando tenga que trabajar con la info-producto
 */
         htmlContentToAppend += `
                 <div onclick="setProductID(${producto.id})" class="list-group-item list-group-item-action cursor-active">
@@ -43,13 +53,19 @@ function setProductID(id) {
     document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
 }
     
-
+/*al cargar la página (el DOM)
+llama a getJsonData y el objeto que me devuelve lo guardo en el array listado
+luego llama a la función mostarProductos 
+que lo hace a traves de un string en el cual va cargando cada uno de los elementos
+recorriendo el array con un for
+y luego con dom los va dibujando en pantalla*/
 document.addEventListener("DOMContentLoaded", function () {
     getJSONData(listaPRODUCTO_URL).then(resultObj => {
-        document.getElementById("aquiVeras").innerText = resultObj.data.catName;//para que muestre catName en el subtitulo
+        //para que muestre catName en el subtitulo
+        document.getElementById("aquiVeras").innerText = resultObj.data.catName;
         if (resultObj.status == "ok") {
             listado = resultObj.data.products;
-            mostrarProductos(listado);
+            mostrarProductos();
         } else {
             alert("no se puede mostrar el contenido");
         }
